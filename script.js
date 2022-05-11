7d9a8a2cb1e5d317feb3779290885651c5865336
Yoel Melki <ymelki@gmail.com>
2022-05-11 15:19:15 +0200

    votre message

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-alert(" Vous êtes bien sur notre site ! ");
+// LA PARTIE DU DOM : POUR MODIFIER LA PARTIE HTML LORS D EVENEMENTS 
+
+// on a appris à modifier une div
+// document.getElementById("madiv") : qui va recuperer l'emplacement de la div
+// innerHTML="le nouveau text"; qui modifier le HTML de la div
+document.getElementById("madiv").innerHTML="le nouveau text";
+
+/* ALGORITHMIQUE
+
+1 VARIABLES
+Lorsque on va stocke 
+ou manipulé on va avoir besoin de données on va mettre dans une variables
+
+2 CONDITIONS
+
+3 BOUCLES
+
+4 FONCTION
+
+*/
+// VARIABLES NUMERIQUE
+// CREATION DE LA VARIABLE NUMERIQUE AGE CONTENANT 34
+age=34;
+alert(age);
+
+// VARIABLES CHAINE DE CARACTERE
+// CREATION DE LA VARIABLE CHAINE DE CARACTERE NOM CONTENANT MELKI
+nom="Melki";
+alert(nom);
+
