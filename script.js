c68f58cf364c0b575c4a854faad2ef7e414604cb
Yoel Melki <ymelki@gmail.com>
2022-05-11 16:56:30 +0200

    votre message

--- script.js
+++ script.js
@@ -18,13 +18,43 @@ ou manipulé on va avoir besoin de données on va mettre dans une variables
 4 FONCTION
 
 */
-// VARIABLES NUMERIQUE
+
+/* 1 LES VARIALES */
+// VARIABLES NUMERIQUE SANS GUILLEMENT
 // CREATION DE LA VARIABLE NUMERIQUE AGE CONTENANT 34
-age=34;
+age=15;
 alert(age);
 
-// VARIABLES CHAINE DE CARACTERE
+// VARIABLES CHAINE DE CARACTERE AVEC GUILLEMENT
 // CREATION DE LA VARIABLE CHAINE DE CARACTERE NOM CONTENANT MELKI
 nom="Melki";
 alert(nom);
 
+
+/* 2 LES CONDITIONS */
+if (age > 17 ) {
+    alert("Vous êtes majeur ! ");
+}
+if (age < 18 ) {
+    alert("Vous êtes mineur ! ");
+}
+
+/* 3 LES BOUCLES */
+// objectif : affiche 10 fois l'alerte : je suis heureux X fois !
+// ON DEMMARE la variable I A 0;ON S ARRETTE A 10;A CHAQUE FOIS ON INCREMENT DE 1
+/*for (i=0;i<2;i++) {
+    alert("Je suis heureux !");
+}*/
+
+/* Les fonctions */
+// SEGMENTER SON CODE
+// ON VA AVOIR DU CODE QUI VA ETRE DANS UNE FONCTION
+
+// 1. CREATION DU CORP DE LA FONCTION
+function blabla(){
+    // DEBUT DE CODE  300 LIGNES
+    alert("je suis dans la fonction VOILA CE QU IL SE PASSE! ");
+}
+
+// 2. l appel de la fonction
+blabla()
